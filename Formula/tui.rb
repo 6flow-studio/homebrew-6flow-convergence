16ad9b0a4c8137674a984bd5504dbc149dcc8985
nonkung51 <non_thakon@hotmail.com>
2026-02-26 14:35:12 +0700

    Fix typo: convergence

--- Formula/tui.rb
+++ Formula/tui.rb
@@ -1,8 +1,8 @@
 class Tui < Formula
   desc "6Flow terminal UI"
-  homepage "https://github.com/6flow-studio/6flow-convegence"
-  url "https://github.com/6flow-studio/6flow-convegence/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "90c302b7e5aaba71654e4c270ba55782c7ca47505ba523225d4261174015fb75"
+  homepage "https://github.com/6flow-studio/6flow-convergence"
+  url "https://github.com/6flow-studio/6flow-convergence/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "df3b36f6f7a7abae28b14d692fdd34ea92f35a7e611053a88af52d6e75096d0d"
 
   depends_on "go" => :build
 
