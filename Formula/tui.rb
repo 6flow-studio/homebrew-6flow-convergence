4925a2f4eaa1671c0634936360c5da9f0b4d99d0
Nonthakon Jitchiranant <non_thakon@hotmail.com>
2026-03-05 16:02:43 +0700

    bump version to 0.0.2

--- Formula/tui.rb
+++ Formula/tui.rb
@@ -1,8 +1,8 @@
 class Tui < Formula
   desc "6Flow terminal UI"
   homepage "https://github.com/6flow-studio/6flow-convergence"
-  url "https://github.com/6flow-studio/6flow-convergence/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "df3b36f6f7a7abae28b14d692fdd34ea92f35a7e611053a88af52d6e75096d0d"
+  url "https://github.com/6flow-studio/6flow-convergence/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "d15b2d4f5e440959c4d589fdac9919d51ea74e995dd85b09816524a93411952d"
 
   depends_on "go" => :build
 
